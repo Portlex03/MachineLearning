38511372a3632a59063d887eebc4c9147880c1c2
Portlex <portnyagin924@gmail.com>
2024-01-04 21:46:00 +0600

    Сделанная ргр

---
+++ Notebooks/Streamlit_Rgr/Autor_info.py
@@ -0,0 +1,20 @@
+""" Задание:
+  - страница 1 с информацией о разработчике моделей ML:
+    @ ФИО, 
+    @ номер учебной группы
+    @ цветная фотография, 
+    @ тема РГР)
+"""
+# source venv/Scripts/activate
+# streamlit run Notebooks/Streamlit_Rgr/Autor_info.py
+
+import streamlit as stm 
+  
+stm.set_page_config(page_title = "DevelopInfo") 
+stm.title("Информация об авторе")
+stm.markdown('''
+  ### Имя: Портнягин Алексей
+  ### Группа: ФИТ-222
+  ### Тема РГР: Разработка дашборда \
+  для вывода моделей ML и анализа данных
+''')
